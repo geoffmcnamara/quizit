3a9f48761f137e1797f4d66161768c1b10806c75
Geoff McNamara <geoff.mcnamara@gmail.com>
2019-03-04 08:39:55 -0500

    Lots of changes to quizzes and app, added Review edit and other features

--- quizit.py
+++ quizit.py
@@ -16,8 +16,11 @@
 #         ques_type can be blank (simple one answer question), 
 #            multi_choice, # choices must follow note: multi_choice can be multi-choice or even multichoice
 #            riddle, joke  # no score - a bonus is given if it is answered correctly
+#     Note: you can escape a ";" with "\;" <-- it will not be considered a delimiter but will still get printed as ";"
+#           Also - trailing comments will get ignored, so will blank lines and comment lines
+#           If the trailing comment has '[h]' in it then everything after the '[h]' will be treated as a hint... 
 # Example: DAT_FILE
-# Take the number 5 and add 6 and what is the answer?;11
+# Take the number 5 and add 6 and what is the answer?;11 # only accept 11 [h] The acceptible answer is greater than 10 but less than 15
 # Now multiply that number by 5 and what is the answer?;55; depends
 # There is a house with four walls. All of the walls are facing south. A bear is circling the house. What color is the bear?;white; riddle
 # A fool thinks himself to be wise, but a wise man knows himself to be a fool. Who said this? ; 2 ; multi_choice; Abraham Lincoln; William Shakespeare; Benjamin Franklin; John F Kennedy 
@@ -25,15 +28,17 @@
 # Joke: Why is 6 afraid of 7 ?; because 7, 8, 9!; joke  
 # ############################
 """
-Usage: my_program.py [-hEcf FILE] [-e DAT_FILE] [-u USER] [(-r | -r NUM)] 
+Usage: my_program.py [-hREcf FILE] [ (-e | -e DAT_FILE) ]  [-u USER] [(-r | -r NUM)] 
 
 -h --help    show this
+-R           to select a DAT_FILE and view it for review
 -f DAT_FILE  specify test data file 
 -E           edit this program
--e DAT_FILE  edit the DAT_FILE
+(-e | -e DAT_FILE)  edit the DAT_FILE (looks in the config var QUIZ_FILE_DIR for the DAT_FILE)
+             if no DAT_FILE is given then user will be offered a selection of existing dat files
 -c           animated celebrate (happens on random successes)
 -u USER      specify username
-(-r NUM | -r)      randomize questions , optionally NUM times
+(-r NUM | -r)      randomize questions , optionally NUM times defaults to hitting a the questions once in random order
 
 Notes:
     The DAT_FILE uses a ";" as a delimiter for the fields
@@ -42,17 +47,26 @@ Notes:
         or
     The question ; answer ; question_type 
         or
-    The question ; answer ; multi-choice ; choice1; choice2; ...
+    The question ; answer ; multichoice ; choice1; choice2; ...
     ---
     ques_types can be:
         joke : answer is checked and a bonus credit is added to the score if correct
         riddle : answer is checked and a bonus credit is added to the score if correct
         gimmie : answer is *not* checked but a bonus credit is still added to the score if correct
         depends : used to tell this application not to include this question in a random selection (becuase it depends on the previous question and answer)
-        multi-choice : used for multiple choice questions, choice options must follow 
-            You can also use "multiple choice", "multichoice", multi_choice"
+        multichoice : used for multiple choice questions, choice options must follow it. Choices should be separated by ";"
+            You can also use "multiple choice", "multichoice", multi_choice", "multi-choice"
             Also the choices can be separated by "," but this will fail if any of your choices have a comma in them : eg 12,000
+    If a line in the DAT_FILE is blank it will be ignored
     If a line in the DAT_FILE begins with "#" it will be ignore and treated as a comment
+    If a line ends with a comment indicated with '#' then it will be dropped except....
+        anything after '[h]' in a trailing comment will be considered an available hint
+    Example DAT_FILE lines:
+    # this is strictly a comment
+    4 * 6 = ; 24 # this is a trailing comment that has a hint [h] The answer is more than 20 but less than 30
+    What is the speed of light; 2 ; multichoice; 186 ft/sec; 186,000 miles/sec; 186 miles/sec # be very careful of the semicolons and the commas [h] The answer is probably the fastest choice
+    # an question can contain a semicolon but it must be escaped with '\'
+    "Let all your things have their places\; let each part of your business have its time." represents _______; order # one of Ben Franklin's 13 virtues
 """
 
 # ### imports ### #
@@ -66,10 +80,14 @@ import sys
 import os
 from os.path import expanduser
 # sys.path.append("/home/geoffm/dev/python/gmodules/")
-from gtools import boxit, run_cmd, select_file, file_exists, cls
+from gtools import boxit, run_cmd, select_file, file_exists, cls, path_to
 from math import ceil
 import random
 import re
+try:
+    from dbug import dbug
+except ImportError:
+    pass
 
 
 # ### globals - config ### #
@@ -178,7 +196,7 @@ def showpics(dir='/home/geoffm/Pictures',num=1):
 
 
 # ########################################################
-def request_response(rspns="What is your response [q=quit]? "):
+def request_response(rspns="What is your response? ", hint=""):
     # ####################################################
     """
     purpose: prints out a question and gets a response
@@ -193,6 +211,10 @@ def request_response(rspns="What is your response [q=quit]? "):
     'test only'
     """
     printit("-" * 40, center=True)
+    if len(hint) > 1:
+        rspns += " [q=quit, h=hint]? "
+    else:
+        rspns += " [q=quit]? "
     if rspns == "testing only":
         ans = "test only"
         printit("What is your response? ", center=True)
@@ -205,6 +227,11 @@ def request_response(rspns="What is your response [q=quit]? "):
     # print("ans: " + ans)
     if ans.lower() == "q" or ans.lower() == "quit":
         sys.exit()
+    if ans.lower() == 'h':
+        printit("Hint:" + hint, center=True)
+        ans = request_response()
+
+        
     # else:
     #     print("ans: [" + ans + "] which apparently is != [q]")
     printit("-" * 40, center=True)
@@ -265,8 +292,7 @@ def multi_choice(elems):
     for choice in choices:
         printit(choice, x=ceil((int(columns) - max_choice_len)/2))
     print("\n")
-    ans = request_response()
-    return ans
+    return  # ans
 
 
 # ##############
@@ -292,15 +318,14 @@ def celebrate():
     if args['-c']:
         if file_exists(celebrate_file):
                 cf_lines = open(celebrate_file).read().splitlines()
-                cf_lines = [ l for l in cf_lines if not l.startswith("#") ]
+                cf_lines = [l for l in cf_lines if not l.startswith("#")]
                 line = random.choice(cf_lines)
-                if "skip" not in line.lower() or not bool(re.search('[a-zA-Z]',line)):
-                    # skipped lines gives the effect of random celebration rewards
+                if bool(re.search('[a-zA-Z]', line)):  # blank lines will get skipped
                     run_cmd(line)
     print("\n")
     msg = "You did great!"
-    sayit(msg,prnt=False)
-    boxit(msg,center=True)
+    sayit(msg, prnt=False)
+    boxit(msg, center=True)
     end_time = time.time()
     celebrate_time = end_time - begin_time
     return celebrate_time
@@ -326,20 +351,32 @@ def chk_syntax(lines):
     """
     for line in lines:
         line = line.strip()
-        semicolon_cnt = sum(map(lambda x : 1 if ';' in x else 0, line)) 
+        # purify the line - if blank, ignore, and strip off any comments
+        line = line.partition('#')[0]  # this ignores trailing comments - turns the line blank if it is all a comment line
+        if not line.strip(): # if the line is empty.. empty string is a False value
+            # dbug("found a blank line")
+            continue
+        semicolons = re.findall(r"(?<!\\);",line)
+        # dbug("len(semicolons): " + str(len(semicolons))
+        # semicolon_cnt = sum(map(lambda x : 1 if ';' in x else 0, line)) 
+        semicolon_cnt = len(semicolons)
         if semicolon_cnt < 1:
-            print("Syntax of line: [" + line + "] appears incorect... no [;] found...")
+            print("Syntax of line appears incorrect with no separator [;] found: \n" + line + "\n")
             return False
-        elems = line.split(";")
+        # elems = line.split(";")
+        elems = re.split(r'(?<!\\);', line)
+        # dbug("elems: " + str(elems))
         ques = elems[0].strip()
         ans = elems[1].strip()
         if ans == "":
             print("Problem with provided answer [" + str(ans) +"]")
+            print("This line has a problem:\n" + line)
+            print("Perhaps it is a problem with provided answer [" + str(ans) +"]")
             return False
         if semicolon_cnt > 1:
             ques_type = elems[2].strip()
             if ques_type not in ques_types:
-                print("Question type does not appear to be correct [" + ques_type + "]")
+                print("Question does not appear to be correct: \n" + line + "\n")
                 return False
             if "multi" in ques_type:
                 if semicolon_cnt < 3:
@@ -413,22 +450,22 @@ def main():
             print("... no usable quiz file declared ... exiting...")
             sys.exit()
     if not chk_syntax(lines):
-        print("Syntax of file: [" + DAT_FILE + "] failed... please investigate.")
-        ans = input("Do you want to edit this file [" + DAT_FILE + "]? ")
+        # print("Syntax of file: [" + DAT_FILE + "] failed... please investigate.")
+        ans = input("Do you want to edit this file [" + DAT_FILE + "] (y|n)? ")
         if "y" in ans.lower():
             do_edit(DAT_FILE)
         sys.exit()
     # ### USER ### #
     global USER
     if USER == "none" and FORCE_NAME:
-        print("\n"*2)
+        # print("\n"*1)
         try:
             # USER = input("Please enter your name: ")
             msg = "Please enter your name: "
             USER = input(printit(msg, center=True, invisable=True))
         except KeyboardInterrupt:
             sys.exit()
-        print("\n"*2)
+        print("\n"*1)
     # ### processall the lines in the DAT_FILE ### #
     BASENAME_DAT_FILE = os.path.basename(DAT_FILE)
     title = "Quizit [version: " + __version__ + "]: " + BASENAME_DAT_FILE
@@ -436,78 +473,96 @@ def main():
         title += " USER: " + USER
     # ### read in the DAT_FILE ### #
     ques_cnt = 0
+    scored_ques_cnt = 0
     score = 0
     # first clear the screen
     cls()
     boxit(title, center=True)
+    # dbug("just did boxit(title, center=True)")
     # ### begin loop of lines in DAT_FILE ### #
     for line in lines:
+        hint = ""
+        if "[h]" in line.partition('#')[2]:
+            hint = line.partition('[h]')[2]
+        line = line.partition('#')[0]  # this ignores trailing comments - turns the line blank if it is all a comment line
+        if not line.strip(): # if the line is empty.. empty string is a False value
+            # dbug("found a blank line")
+            continue
         ques_type = ""
-        elems = line.split(";")
+        ques_cnt += 1
+        scored_ques_cnt += 1  # this will get decremented with non-scored questions
+        # elems = line.split(";")
+        elems = re.split(r'(?<!\\);', line)
         if len(elems) > 2:
             ques_type = str(elems[2].strip().lower())
         if args['-r']:
             line = random.choice(lines)
             if ques_type == "depends":  # or line.startswith("#"):
+                # print("debug: ques_type: " + ques_type)
                 continue
             if args['NUM'] is not None:
                 if ques_cnt >= int(args['NUM']):
                     break
-            elems = line.split(";")  
-        ques_cnt += 1
-        print("\n" * 2 )
+            # elems = line.split(";")  
+            elems = re.split(r'(?<!\\);', line)
+        # ques_cnt += 1
+        # dbug("here now")
+        # print("\n" * 2 )
         msgs = []
         msgs.append("Question: " + str(ques_cnt))
-        ques = elems[0]
+        ques = elems[0].replace("\\;",";")
         msgs.append("-" * 20)
         msgs.append(ques)
-        right_answer = elems[1]
-        boxit(msgs,center=True,y=5)
+        try:
+            right_answer = elems[1]
+        except Exception as e:
+            dbug("All elems: " + str(elems))
+            sys.exit()
+        boxit(msgs,center=True,y=1)
         if len(elems) > 2:
             ques_type = str(elems[2].strip().lower())
             if ques_type in multi_choice_types:
-                ans = multi_choice(elems)
+                multi_choice(elems)  # lists all the choices
             elif ques_type == "gimmie":
-                # the user will be out right given a point in the score as a bonus
-                score += 1
+                 # the user will be out right given a point in the score as a bonus
+            #     score += 1
                 printit("-" * 40, center=True)
                 printit("Your answer will be scored as a bonus ... !", say=True, center=True)
-                ans = request_response()
-                ques_cnt -= 1
+                scored_ques_cnt -= 1
             elif ques_type == "joke" or ques_type == "riddle":
-                # no score change
+                # make this a bonus question
                 printit("-" * 40, center=True)
                 printit("Your answer will be scored as a bonus ... !", say=True, center=True)
-                ans = request_response()
-                ques_cnt -= 1
+                scored_ques_cnt -= 1
             else:
                 if ques_type not in ques_types:
                     printit("Possible error in DAT_FILE: Unknown question type: " + ques_type)
-                ans = request_response()
-                ques_cnt += 1
-        else:  # no ques_type declared - simple answer question
-            ans = request_response()
-        if check_answer(ans, right_answer):  # WIP
+                # ques_cnt += 1
+        ans = request_response(hint=hint)
+        if check_answer(ans, right_answer) or ques_type == "gimmie":
             remove_time += celebrate()
             score += 1
         else:
             disappoint()
         pcnt = str(round(score*100/ques_cnt,2))
-        printit("Score: " + str(score) + " out of " + str(ques_cnt) + " Your percent correct = " + str(round(score*100/ques_cnt,2)) +"%", center=True)
+        print("\n"*1)
+        printit("Score: " + str(score) + " out of " + str(scored_ques_cnt) + " scored questions.", center=True)
+        printit("Your percent correct = " + str(round(score*100/scored_ques_cnt,2)) +"%", center=True)
     printit("=" * 40, center=True)    
     end = time.time()
     elapsed_time = (end - start) - remove_time
-    msg = f"The elapsed time: {(elapsed_time):0.2f} seconds (minus celebration time)"
+    msg = "The elapsed time: {:0.2f} seconds (minus celebration time)".format(elapsed_time)
     printit(msg, center=True, say=True)
     if USER != "none":  # This should always be the case if FORCE_USER is true
         # assuming QUIZ_FILE_DIR has been created at the beginning of main()
-        msg = f"User: {USER:15}, Dtime: {dtime}, DatFile: {BASENAME_DAT_FILE:15}, Score: {pcnt:>6}%, Elapsed_time: {end-start:0.2f}, Num ques: {ques_cnt:6}"
+        # msg = f"User: {USER:15}, Dtime: {dtime}, DatFile: {BASENAME_DAT_FILE:25}, Score: {pcnt:>6}%, Elapsed_time: {end-start:0.2f}, Num ques: {ques_cnt:6}"
+        msg = "User: {:15}, Dtime: {}, DatFile: {:25}, Score: {:>6}%, Elapsed_time: {:0.2f}, Num ques: {:6}".format(USER,dtime,BASENAME_DAT_FILE,pcnt,end-start,ques_cnt)
         global OUT_FILE
         with open(OUT_FILE,'a') as f:
             f.write(msg + "\n")
         print("\n")
         printit("Wrote results into: " + OUT_FILE, say=False, center=True)
-        print("\n"*4)
+        print("\n"*2)
     # EOB #
 
 
@@ -519,9 +574,11 @@ def do_edit(file, lnum=0):
     Initiate edit on a file - with lineno if provided
     """
     if lnum:
-        cmd = f"vim {file} +{str(lnum)}"
+        # cmd = f"vim {file} +{str(lnum)}"
+        cmd = "vim " + file + " " + str(num)
     else:
-        cmd = f"vim {file}"
+        # cmd = f"vim {file}"
+        cmd = "vim " + file
     r = subprocess.call(cmd, shell=True)
     return r
 
@@ -532,11 +589,45 @@ def handleOPTS(args):
     """
     purpose: handles options
     """
+    # print("args: " + str(args))
     if args['-E']:
         do_edit(__file__)
         sys.exit()
     if args['-e']:
-        do_edit(args['-e'])
+        if args['DAT_FILE'] is not None:
+            do_edit(QUIZ_FILE_DIR + "/" + args['DAT_FILE'])
+        else:
+            dat_file = select_file(QUIZ_FILE_DIR, pattern="*.dat", box=True, center=True)
+            do_edit(QUIZ_FILE_DIR + "/" + dat_file)
+        sys.exit()
+    if args['-R']:
+        dat_file = select_file(QUIZ_FILE_DIR, pattern="*.dat", box=True, center=True)
+        rows, columns = os.popen('stty size', 'r').read().split()
+        rows = int(rows)
+        cols = int(columns)
+        #  geom = COL, ROWS, x, y)
+        geom = str(cols) + "x" + str(rows) + "+30+30"
+        cmd = "'less " + QUIZ_FILE_DIR + "/" + dat_file + "'"
+        # this will open an xterm as big as the current one with top left screen location 30+30
+        term_cmds = {
+                    "tilix":" --geometry=" + geom + " -e " + cmd,
+                    "xterm":" -fa 'monospace' -fs 14 -g " + geom + ' -e ' + cmd,
+                    "terminator":" -e " + cmd + " --geometry=1200x800+30+30",
+                    "gnome-terminal":" --geometry=" + geom + " -e " + cmd,
+                    "rxvt-unicode":" --geometry=" + geom + " -e " + cmd,
+                    "eterm":" --geometry=" + geom + " -e " + cmd,
+                    "Xfce4-terminal":" --geometry=" + geom + " -e " + cmd,
+                    "termite":" --geometry=" + geom + " -e " + cmd,
+                    }
+        print("=" * 40)
+        for k,v in term_cmds.items():
+            if path_to(k):
+                full_path = path_to(k)
+                full_cmd = full_path + v
+                print("Running: " + full_cmd )
+                run_cmd(full_cmd)
+                break  # found a working term --- done!
+        print("=" * 40)
         sys.exit()
     if args['-f']:
         global DAT_FILE
